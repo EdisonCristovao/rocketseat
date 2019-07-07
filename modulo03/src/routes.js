dcaba938ad24ec7f4efe947f9193cd7995ffef53
edisoncristovao <edison.cr.jr@gmail.com>
2019-07-07 12:15:17 -0300

    start modulo03

---
+++ modulo03/src/routes.js
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import authMidleware from './app/midlewares/auth';
+
+const routes = new Router();
+
+routes.post('/users', UserController.store);
+routes.post('/session', SessionController.store);
+
+routes.use(authMidleware);
+
+routes.put('/users', UserController.update);
+export default routes;
