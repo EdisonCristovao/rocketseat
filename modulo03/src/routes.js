36860a28ee98866b616cf7050f82502d8d5be48a
edisoncristovao <edison.cr.jr@gmail.com>
2019-07-18 11:10:24 -0300

    list appointments available

--- modulo03/src/routes.js
+++ modulo03/src/routes.js
@@ -8,6 +8,7 @@ import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
 
 import authMidleware from './app/midlewares/auth';
 import internalError from './app/midlewares/internalError';
@@ -26,6 +27,7 @@ routes.use(authMidleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
