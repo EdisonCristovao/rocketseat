693d3f4cb198f473ad1e0e4452368f872416822a
edisoncristovao <edison.cr.jr@gmail.com>
2019-07-24 21:51:27 -0300

    start styled component

---
+++ modulo05/src/pages/Main/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Title } from './styles';
+
+function Main() {
+  return (
+    <Title error>
+      Main
+      <small>menor</small>
+    </Title>
+  );
+}
+
+export default Main;
