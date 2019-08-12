49f1cc475a2e573185123165f8b954b4cc2db784
Edison Cristovao <edison.cr.jr@gmail.com>
2019-08-11 22:49:49 -0300

    header ok

--- modulo07/src/components/Header/index.js
+++ modulo07/src/components/Header/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { MdShoppingBasket } from 'react-icons/md';
 
-import { Container } from './styles';
+import { Container, Cart } from './styles';
+
+import logo from '../../assets/images/logo.svg';
 
 export default function Header() {
-  return <Container > <img src={} alt="rocketShoes"></img></Container>;
+  return (
+    <Container>
+      <Link to="/">
+        <img src={logo} alt="rocketShoes" />
+      </Link>
+
+      <Cart to="/cart">
+        <div>
+          <strong>Meu carrinho</strong>
+          <span>3 itens</span>
+        </div>
+
+        <MdShoppingBasket size={36} color="#FFF" />
+      </Cart>
+    </Container>
+  );
 }
